a1cacd63f29dc79885f39fdd526d23e7b815ec1d
Emad Jammal <emad.elqassas.1@gmail.com>
2023-08-17 06:53:07 +0300

    weather assignment

--- JS/main.js
+++ JS/main.js
@@ -9,36 +9,77 @@ let temp = document.querySelector("#temp");
 let conSearch = document.querySelector("#conSearch");
 let find = document.querySelector("#find");
 let img = document.querySelector("#img");
-let allData = [];
+let statu = Array.from(document.querySelectorAll(".statu"));
+let statuImg = Array.from(document.querySelectorAll(".statuImg"));
+let tempeture = Array.from(document.querySelectorAll(".tempeture"));
+let maxTemp = Array.from(document.querySelectorAll(".maxTemp"));
+let timeTemp = Array.from(document.querySelectorAll(".timeTemp"))
+let allData ;
+
 contact.addEventListener("click", ()=> {
   home.classList.add("d-none");
   contactUs.classList.remove("d-none");
-})
+});
+
 weather.addEventListener("click", ()=> {
   contactUs.classList.add("d-none");
   home.classList.remove("d-none");
-})
+});
+
 find.addEventListener("click",()=> {
   display(conSearch.value)
-})
+});
+
 let req = new XMLHttpRequest();
+
 function display(searchKey){
-  req.open("GET",`https://api.weatherapi.com/v1/forecast.json?key=87695f0dbfb24656a5f215528231408&q=${searchKey}&days=3`);
-req.send();
-req.addEventListener("readystatechange",()=> {
+  req.open("GET",`https://api.weatherapi.com/v1/forecast.json?key=87695f0dbfb24656a5f215528231408&q=${searchKey}&days=7`);
+  req.send();
+  req.addEventListener("readystatechange",function () {
+
   if (req.status == 200 && req.readyState == 4){
     let posts = JSON.parse(req.response);
     console.log(posts);
-    posts.location.localtime = "2023-08-16 5:52";
-    console.log(posts.location.localtime);
+    for (let i = 0; i <posts.forecast.forecastday.length ; i++) {
+      
+      let {avgtemp_c} = posts.forecast.forecastday[i].day
+      let {maxtemp_c} = posts.forecast.forecastday[i].day
+      let {text} = posts.forecast.forecastday[i].day.condition
+      let {moonrise,moonset,sunrise,sunset} = posts.forecast.forecastday[i].astro
+      let time = [sunrise,moonrise,sunset,moonset,]
+      for (let i = 0; i < time.length ; i++) {
+        timeTemp[i].innerHTML = time[i];
+      }
+      if (text == "Patchy rain possible" || text == "Heavy rain" || text == "Moderate rain") {
+        text = "Rainy"
+      }
+      if (text == "Partly cloudy" || text == "" || text == "") {
+        text = "Cloudy"
+      }
+      statu[i].innerHTML = text;
+      maxTemp[i].innerHTML = `/${Math.ceil(maxtemp_c)}`;
+      tempeture[i].innerHTML = avgtemp_c;
+
+      if (text == "Rainy"){
+        statuImg[i].setAttribute("src","img/3d-cartoon-weather-icons-set-sun-rain-cloud-lightning-thunderstorm_363543-504.png");
+      }else if(text == "Cloudy") {
+        statuImg[i].setAttribute("src","./img/cartoon-clouds-isolated-blue-background-cumulus-fluffy-eddy-blue-sky-hand-drawn-sketch_890748-565.png");
+      } else if (text == "Sunny") {
+        statuImg[i].setAttribute("src","./img/vector-modern-flat-sun-sunshine-icon-yellow-gradient-sunhot-weather-icon_491392-71.png")
+      }
+
+    }
+
     Name.innerHTML = posts.location.name;
     temp.innerHTML = posts.current.temp_c
     if (posts.current.temp_c <= 20){
-      img.setAttribute("src","./img/3d-cartoon-weather-icons-set-sun-rain-cloud-lightning-thunderstorm_363543-504.png")
+      img.setAttribute("src","./img/3d-cartoon-weather-icons-set-sun-rain-cloud-lightning-thunderstorm_363543-504.png");
     }else if(posts.current.temp_c > 20 && posts.current.temp_c <= 30) {
-      img.setAttribute("src","img/=kk==.png")
+      img.setAttribute("src","img/=kk==.png");
     }
   }
-})
+
+});
 }
 display("alex")
+
